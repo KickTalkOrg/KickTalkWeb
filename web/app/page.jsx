e6446af4fba294d3c26c77c130d66c12ac3bdaf8
Dark <124302864+thedrkness@users.noreply.github.com>
2025-05-06 14:47:55 -0700

    Update page.jsx

--- web/app/page.jsx
+++ web/app/page.jsx
@@ -2,123 +2,114 @@
 
 import Image from "next/image";
 import "./page.css";
-import { useState } from 'react';
+import { useState } from "react";
 import logo from "../public/logo.svg";
 import card1 from "../public/app_screenshots/card-1.png";
 import card2 from "../public/app_screenshots/card-2.png";
 import card3 from "../public/app_screenshots/card-3.png";
 import card4 from "../public/app_screenshots/card-4.png";
 import card5 from "../public/app_screenshots/card-5.png";
-import Loader from './loader';
-
+import Loader from "./loader";
 
 export default function Home() {
   const [isLoaded, setIsLoaded] = useState(false);
   return (
     <>
-    {!isLoaded && <Loader onFinish={() => setIsLoaded(true)} />}
-  
-    <div className="homeContainer">
-      <div className="homeHeader">
-        <div className="homeHeaderLeft">
-          <div className="homeHeaderLeftLogo">
-            <Image src={logo} alt="logo" priority width={100} height={100} />
+      {!isLoaded && <Loader onFinish={() => setIsLoaded(true)} />}
+
+      <div className="homeContainer">
+        <div className="homeHeader">
+          <div className="homeHeaderLeft">
+            <div className="homeHeaderLeftLogo">
+              <Image src={logo} alt="logo" priority width={100} height={100} />
+            </div>
+          </div>
+
+          <div className="homeHeaderRight">
+            <div className="homeHeaderRightCreators">
+              <p>
+                Created by{" "}
+                <a href="https://x.com/drkerco" target="_blank" rel="noopener noreferrer">
+                  DRKNESS
+                </a>{" "}
+                and{" "}
+                <a href="https://x.com/ftk789yt" target="_blank" rel="noopener noreferrer">
+                  ftk789
+                </a>
+              </p>
+            </div>
           </div>
         </div>
 
-        <div className="homeHeaderRight">
-          <div className="homeHeaderRightCreators">
-            <p>
-              Created by{" "}
-              <a href="https://x.com/drkerco" target="_blank" rel="noopener noreferrer">
-                DRKNESS
-              </a>{" "}
-              and{" "}
-              <a href="https://x.com/ftk789yt" target="_blank" rel="noopener noreferrer">
-                ftk789
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
+        <div className="homeDivider" />
 
-      <div className="homeDivider" />
+        <div className="homeBody">
+          <div className="homeBodyHead">
+            <div className="homeBodyHeadLeft">
+              <div className="homeBodyHeadLeftTitle">
+                <span>KickTalk</span>
+                <h1>A Kick Chat client to elevate your chatting experience.</h1>
+              </div>
+            </div>
 
-      <div className="homeBody">
-        <div className="homeBodyHead">
-          <div className="homeBodyHeadLeft">
-            <div className="homeBodyHeadLeftTitle">
-              <span>KickTalk</span>
-              <h1>A Kick Chat client to elevate your chatting experience.</h1>
+            <div className="homeBodyHeadRight">
+              <button
+                className="homeBodyHeadRightButton download"
+                onClick={() => window.open("https://kick-talk-beta.replit.app/KickTalkBetaTest.exe", "_blank")}>
+                Download the Beta
+              </button>
+              <button
+                className="homeBodyHeadRightButton discord"
+                onClick={() => window.open("https://discord.gg/nFrZaxJmdn", "_blank")}>
+                Join our Discord
+              </button>
             </div>
           </div>
 
-          <div className="homeBodyHeadRight">
-            <button
-              className="homeBodyHeadRightButton download"
-              onClick={() => window.open("https://api.kicktalk.app/download", "_blank")}>
-              Download the Beta
-            </button>
-            <button
-              className="homeBodyHeadRightButton discord"
-              onClick={() => window.open("https://discord.gg/nFrZaxJmdn", "_blank")}>
-              Join our Discord
-            </button>
-          </div>
-        </div>
+          <div className="homeBodyScreenshots">
+            <div className="screenshotItem">
+              <Image src={card1} alt="" />
 
-        <div className="homeBodyScreenshots">
-          <div className="screenshotItem">
-            <Image src={card1} alt="" />
-
-            <div className="homeBodyScreenshotsFooter">
-              <h3>Settings Dialog</h3>
-              <span>Customize your experience and how chat looks with KickTalk.</span>
+              <div className="homeBodyScreenshotsFooter">
+                <h3>Settings Dialog</h3>
+                <span>Customize your experience and how chat looks with KickTalk.</span>
+              </div>
             </div>
-          </div>
-          <div className="screenshotItem">
-            <Image src={card2} alt="" />
+            <div className="screenshotItem">
+              <Image src={card2} alt="" />
 
-            <div className="homeBodyScreenshotsFooter">
-              <h3>Smooth UI</h3>
-              <span>
-              Experience a very elegant and a satesfyting UI with KickTalk.
-              </span>
+              <div className="homeBodyScreenshotsFooter">
+                <h3>Smooth UI</h3>
+                <span>Experience a very elegant and a satesfyting UI with KickTalk.</span>
+              </div>
             </div>
-          </div>
-          <div className="screenshotItem">
-            <Image src={card3} alt="" />
+            <div className="screenshotItem">
+              <Image src={card3} alt="" />
 
-            <div className="homeBodyScreenshotsFooter">
-              <h3>User Dialogs</h3>
-              <span>
-              Enjoy reading viewers logs and with KickTalk.
-              </span>
+              <div className="homeBodyScreenshotsFooter">
+                <h3>User Dialogs</h3>
+                <span>Enjoy reading viewers logs and with KickTalk.</span>
+              </div>
             </div>
-          </div>
-          <div className="screenshotItem">
-            <Image src={card4} alt="" />
+            <div className="screenshotItem">
+              <Image src={card4} alt="" />
 
-            <div className="homeBodyScreenshotsFooter">
-              <h3>Native Functionality</h3>
-              <span>
-              You can still use and view Kick’s native functionality with better enhancements with KickTalk.
-              </span>
+              <div className="homeBodyScreenshotsFooter">
+                <h3>Native Functionality</h3>
+                <span>You can still use and view Kick’s native functionality with better enhancements with KickTalk.</span>
+              </div>
             </div>
-          </div>
-          <div className="screenshotItem">
-            <Image src={card5} alt="" />
+            <div className="screenshotItem">
+              <Image src={card5} alt="" />
 
-            <div className="homeBodyScreenshotsFooter">
-              <h3>Emote Dialogs</h3>
-              <span>
-              express your feelings and enjoy picking your favorite emotes with KickTalk.
-              </span>
+              <div className="homeBodyScreenshotsFooter">
+                <h3>Emote Dialogs</h3>
+                <span>express your feelings and enjoy picking your favorite emotes with KickTalk.</span>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
