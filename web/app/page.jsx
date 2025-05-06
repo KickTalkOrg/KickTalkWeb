55085091f879ad7ba63b5045ae9de07355aa8f67
Dark <124302864+thedrkness@users.noreply.github.com>
2025-05-06 11:25:18 -0700

    Update page.jsx

--- web/app/page.jsx
+++ web/app/page.jsx
@@ -1,14 +1,17 @@
+"use client";
+
 import Image from "next/image";
 import "./page.css";
 import logo from "../public/logo.svg";
 import screenshot1 from "@/public/app_screenshots/2.png";
+
 export default function Home() {
   return (
     <div className="homeContainer">
       <div className="homeHeader">
         <div className="homeHeaderLeft">
           <div className="homeHeaderLeftLogo">
-            <Image src={logo} alt="logo" width={100} height={100} />
+            <Image src={logo} alt="logo" priority width={100} height={100} />
           </div>
         </div>
 
@@ -16,11 +19,11 @@ export default function Home() {
           <div className="homeHeaderRightCreators">
             <p>
               Created by{" "}
-              <a href="https://x.com/drkerco" target="_blank">
+              <a href="https://x.com/drkerco" target="_blank" rel="noopener noreferrer">
                 DRKNESS
               </a>{" "}
               and{" "}
-              <a href="https://x.com/ftk789yt" target="_blank">
+              <a href="https://x.com/ftk789yt" target="_blank" rel="noopener noreferrer">
                 ftk789
               </a>
             </p>
