a2d0e48be7aa8645d02a640c31a0994e7fe9ae6b
moath alayel <57598907+ftk789@users.noreply.github.com>
2025-05-07 22:00:33 +0300

    /download redirect

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/download',
+            destination: 'https://kick-talk-beta.replit.app/KickTalkBetaTest.exe',
+            permanent: true,
+          },
+        ]
+      },
+};
 
 export default nextConfig;
