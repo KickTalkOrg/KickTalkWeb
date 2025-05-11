afd4d83697eb73694dc7c8c49abd5b8d8782a1ed
Dark <124302864+thedrkness@users.noreply.github.com>
2025-05-10 17:57:04 -0700

    Update next.config.mjs

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/download",
+        destination: "https://api.kicktalk.app/downloads/KickTalkBetaTest.exe",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
