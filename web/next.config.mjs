f2ce1e6a3e8df55ae6998e6de010522702f74ee0
Dark <124302864+thedrkness@users.noreply.github.com>
2025-05-16 18:50:00 -0700

    Update next.config.mjs

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+
+  redirects: async () => {
+    return [
+      {
+        source: "/download",
+        destination: "/changelogs?version=latest",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
