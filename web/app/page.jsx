506810d8eb4ca01cc7a3b3d161e8aea98416c8fc
moath alayel <57598907+ftk789@users.noreply.github.com>
2025-05-29 20:47:14 +0300

    Made loader better

--- web/app/page.jsx
+++ web/app/page.jsx
@@ -21,167 +21,165 @@ export default function Home() {
     <>
       {!isLoaded && <Loader onFinish={() => setIsLoaded(true)} />}
 
-      {isLoaded && (
-        <div className="homeContainer">
-          <div className="homeHeader">
-            <div className="homeHeaderLeft">
-              <div className="homeHeaderLeftLogo">
-                <Image src={logo} alt="KickTalk Logo" priority width={100} height={100} />
+      <div className="homeContainer">
+        <div className="homeHeader">
+          <div className="homeHeaderLeft">
+            <div className="homeHeaderLeftLogo">
+              <Image src={logo} alt="KickTalk Logo" priority width={100} height={100} />
+            </div>
+          </div>
+          <div className="homeHeaderMiddle">
+            <Link href="/changelogs">
+              <span>Changelogs</span>
+              <div className="homeHeaderMiddleUnderline" />
+            </Link>
+          </div>
+          <div className="homeHeaderRight">
+            <p>
+              Created by{" "}
+              <a href="https://x.com/drkerco" target="_blank" rel="noopener noreferrer">
+                DRKNESS_x
+              </a>{" "}
+              and{" "}
+              <a href="https://x.com/ftk789yt" target="_blank" rel="noopener noreferrer">
+                ftk789
+              </a>
+            </p>
+          </div>
+        </div>
+
+        <div className="homeHeaderBackground" />
+
+        <div className="homeBody">
+          <div className="homeBodyHead">
+            <div className="homeBodyHeadLeft">
+              <div className="homeBodyHeadLeftTitle">
+                <span>KickTalk</span>
+                <h1>A Kick Chat client to elevate your chatting experience.</h1>
               </div>
             </div>
-            <div className="homeHeaderMiddle">
-              <Link href="/changelogs">
-                <span>Changelogs</span>
-                <div className="homeHeaderMiddleUnderline" />
+            <div className="homeBodyHeadRight">
+              <Link className="homeBodyHeadRightButton download" href="/changelogs?version=latest">
+                Download the Beta
+              </Link>
+              <Link className="homeBodyHeadRightButton discord" href="https://discord.gg/nFrZaxJmdn" target="_blank">
+                Join our Discord
               </Link>
             </div>
-            <div className="homeHeaderRight">
-              <p>
-                Created by{" "}
-                <a href="https://x.com/drkerco" target="_blank" rel="noopener noreferrer">
-                  DRKNESS_x
-                </a>{" "}
-                and{" "}
-                <a href="https://x.com/ftk789yt" target="_blank" rel="noopener noreferrer">
-                  ftk789
-                </a>
-              </p>
-            </div>
           </div>
 
-          <div className="homeHeaderBackground" />
-
-          <div className="homeBody">
-            <div className="homeBodyHead">
-              <div className="homeBodyHeadLeft">
-                <div className="homeBodyHeadLeftTitle">
-                  <span>KickTalk</span>
-                  <h1>A Kick Chat client to elevate your chatting experience.</h1>
+          <div className="homeBodyScreenshots">
+            {[
+              {
+                img: card1,
+                title: "Smooth UI",
+                desc: "Experience a very elegant, satisfying and smooth UI that is easy on the eyes.",
+              },
+              {
+                img: card2,
+                title: "Pinned Messages",
+                desc: "View pinned messages in a chatroom. You can also pin your own messages to keep them at the top of the chat.",
+              },
+              {
+                img: card3,
+                title: "User Dialogs",
+                desc: "Click on a user to view their logs in the channel plus mod actions available for channels where youre a mod.",
+              },
+              {
+                img: card4,
+                title: "Colon Completion",
+                desc: "Easily access emote completion using the colon key and tab to autocomplete.",
+              },
+              {
+                img: card5,
+                title: "Emote Dialogs",
+                desc: "Express yourself and enjoy picking your favorite emotes from 7TV to Kick emotes.",
+              },
+              // {
+              //   img: card5,
+              //   title: "7TV Features Built In",
+              //   desc: "7TV emotes, badges, paints and more are now built in and available to use in chatrooms.",
+              // },
+              // {
+              //   // img: card6,
+              //   title: "Reply Threads",
+              //   desc: "Reply to messages and create threads to keep conversations organized. Threads are a custom feature built in only on KickTalk.",
+              // },
+              // {
+              //   // img: card7,
+              //   title: "Chatters List",
+              //   desc: "View a list of chatters in a chatroom. This is a custom feature built in only on KickTalk.",
+              // },
+              // {
+              //   img: card5,
+              //   title: "Emote Dialogs",
+              //   desc: "Express yourself and enjoy picking your favorite emotes from 7TV to Kick emotes.",
+              // },
+              // {
+              //   img: card5,
+              //   title: "Emote Dialogs",
+              //   desc: "Express yourself and enjoy picking your favorite emotes from 7TV to Kick emotes.",
+              // },
+              // {
+              //   img: card5,
+              //   title: "Emote Dialogs",
+              //   desc: "Express yourself and enjoy picking your favorite emotes from 7TV to Kick emotes.",
+              // },
+              // {
+              //   img: card5,
+              //   title: "Emote Dialogs",
+              //   desc: "Express yourself and enjoy picking your favorite emotes from 7TV to Kick emotes.",
+              // },
+              // {
+              //   img: card5,
+              //   title: "Emote Dialogs",
+              //   desc: "Express yourself and enjoy picking your favorite emotes from 7TV to Kick emotes.",
+              // },
+            ].map(({ img, title, desc }, idx) => (
+              <div className="screenshotItem" key={idx}>
+                <Image src={img} alt={title} />
+                <div className="homeBodyScreenshotsFooter">
+                  <h3>{title}</h3>
+                  <span>{desc}</span>
                 </div>
               </div>
-              <div className="homeBodyHeadRight">
-                <Link className="homeBodyHeadRightButton download" href="/changelogs?version=latest">
-                  Download the Beta
-                </Link>
-                <Link className="homeBodyHeadRightButton discord" href="https://discord.gg/nFrZaxJmdn" target="_blank">
-                  Join our Discord
-                </Link>
-              </div>
-            </div>
+            ))}
+          </div>
+        </div>
 
-            <div className="homeBodyScreenshots">
-              {[
-                {
-                  img: card1,
-                  title: "Smooth UI",
-                  desc: "Experience a very elegant, satisfying and smooth UI that is easy on the eyes.",
-                },
-                {
-                  img: card2,
-                  title: "Pinned Messages",
-                  desc: "View pinned messages in a chatroom. You can also pin your own messages to keep them at the top of the chat.",
-                },
-                {
-                  img: card3,
-                  title: "User Dialogs",
-                  desc: "Click on a user to view their logs in the channel plus mod actions available for channels where youre a mod.",
-                },
-                {
-                  img: card4,
-                  title: "Colon Completion",
-                  desc: "Easily access emote completion using the colon key and tab to autocomplete.",
-                },
-                {
-                  img: card5,
-                  title: "Emote Dialogs",
-                  desc: "Express yourself and enjoy picking your favorite emotes from 7TV to Kick emotes.",
-                },
-                // {
-                //   img: card5,
-                //   title: "7TV Features Built In",
-                //   desc: "7TV emotes, badges, paints and more are now built in and available to use in chatrooms.",
-                // },
-                // {
-                //   // img: card6,
-                //   title: "Reply Threads",
-                //   desc: "Reply to messages and create threads to keep conversations organized. Threads are a custom feature built in only on KickTalk.",
-                // },
-                // {
-                //   // img: card7,
-                //   title: "Chatters List",
-                //   desc: "View a list of chatters in a chatroom. This is a custom feature built in only on KickTalk.",
-                // },
-                // {
-                //   img: card5,
-                //   title: "Emote Dialogs",
-                //   desc: "Express yourself and enjoy picking your favorite emotes from 7TV to Kick emotes.",
-                // },
-                // {
-                //   img: card5,
-                //   title: "Emote Dialogs",
-                //   desc: "Express yourself and enjoy picking your favorite emotes from 7TV to Kick emotes.",
-                // },
-                // {
-                //   img: card5,
-                //   title: "Emote Dialogs",
-                //   desc: "Express yourself and enjoy picking your favorite emotes from 7TV to Kick emotes.",
-                // },
-                // {
-                //   img: card5,
-                //   title: "Emote Dialogs",
-                //   desc: "Express yourself and enjoy picking your favorite emotes from 7TV to Kick emotes.",
-                // },
-                // {
-                //   img: card5,
-                //   title: "Emote Dialogs",
-                //   desc: "Express yourself and enjoy picking your favorite emotes from 7TV to Kick emotes.",
-                // },
-              ].map(({ img, title, desc }, idx) => (
-                <div className="screenshotItem" key={idx}>
-                  <Image src={img} alt={title} />
-                  <div className="homeBodyScreenshotsFooter">
-                    <h3>{title}</h3>
-                    <span>{desc}</span>
-                  </div>
-                </div>
-              ))}
-            </div>
+        <div className="homeDivider" />
+
+        <div className="homeModActions">
+          <div className="homeModActionsHeader">
+            <span>Simplifying moderator actions at your finger tips.</span>
+            <h1>Mod Actions</h1>
           </div>
 
-          <div className="homeDivider" />
-
-          <div className="homeModActions">
-            <div className="homeModActionsHeader">
-              <span>Simplifying moderator actions at your finger tips.</span>
-              <h1>Mod Actions</h1>
-            </div>
-
-            <div className="homeModActionsBody">
-              <div className="homeModActionsBodyItem split">
-                <div className="homeModActionsLeft">
-                  <h1 className="homeModActionsTitle">Mod Actions within User Cards</h1>
-                  <div className="homeModActionsLeftList">
-                    <span>Ban Users</span>
-                    <span>Unban Users</span>
-                    <span>Timeout Users</span>
-                    <span>View Chatroom Logs</span>
-                    <span>and more to come...</span>
-                  </div>
-                </div>
-                <div className="homeModActionsRight">
-                  <Image src={modActionsUserCard} alt="Mod Actions in User Cards" />
+          <div className="homeModActionsBody">
+            <div className="homeModActionsBodyItem split">
+              <div className="homeModActionsLeft">
+                <h1 className="homeModActionsTitle">Mod Actions within User Cards</h1>
+                <div className="homeModActionsLeftList">
+                  <span>Ban Users</span>
+                  <span>Unban Users</span>
+                  <span>Timeout Users</span>
+                  <span>View Chatroom Logs</span>
+                  <span>and more to come...</span>
                 </div>
               </div>
-              <div className="homeModActionsBodyItem middle">
-                <h1 className="homeModActionsTitle">Pinning and Unpinning Messages</h1>
-                <div className="homeModActionsMiddle">
-                  <Image src={modActionsPinning} alt="Pinning and Unpinning Messages" />
-                </div>
+              <div className="homeModActionsRight">
+                <Image src={modActionsUserCard} alt="Mod Actions in User Cards" />
+              </div>
+            </div>
+            <div className="homeModActionsBodyItem middle">
+              <h1 className="homeModActionsTitle">Pinning and Unpinning Messages</h1>
+              <div className="homeModActionsMiddle">
+                <Image src={modActionsPinning} alt="Pinning and Unpinning Messages" />
               </div>
             </div>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 }
